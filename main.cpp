707de98bd73ecb70a0af27a32c1e596688625aa4
みうね <54197251+m1une@users.noreply.github.com>
2026-07-16 04:56:59 +0900

    add pch

--- main.cpp
+++ main.cpp
@@ -1,31 +1,10 @@
-#ifdef LOCAL
-#define _GLIBCXX_DEBUG
-#pragma GCC optimize("O0")
-#else
-#pragma GCC optimize("O3")
-#pragma GCC optimize("unroll-loops")
-#endif
+#include "pch.hpp"
 
-#ifdef LOCAL
-// https://zenn.dev/sassan/articles/19db660e4da0a4
-#include "cpp-dump/cpp-dump.hpp"
-#define dump(...) cpp_dump(__VA_ARGS__)
-#else
-#define dump(...)
-#define CPP_DUMP_SET_OPTION(...)
-#define CPP_DUMP_DEFINE_EXPORT_OBJECT(...)
-#define CPP_DUMP_DEFINE_EXPORT_ENUM(...)
-#define CPP_DUMP_DEFINE_DANGEROUS_EXPORT_OBJECT(...)
-#endif
-
-#include "template.hpp"
-
-#include "utilities/fast_io.hpp"
 m1une::utilities::FastInput fastin;
 m1une::utilities::FastOutput fastout;
 
 void solve() {
-    
+
 }
 
 int main() {
---
+++ pch.hpp
@@ -0,0 +1,27 @@
+#ifndef M1UNE_PCH_HPP
+#define M1UNE_PCH_HPP 1
+
+#ifdef LOCAL
+#define _GLIBCXX_DEBUG
+#pragma GCC optimize("O0")
+#else
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+#endif
+
+#ifdef LOCAL
+// https://zenn.dev/sassan/articles/19db660e4da0a4
+#include "cpp-dump/cpp-dump.hpp"
+#define dump(...) cpp_dump(__VA_ARGS__)
+#else
+#define dump(...)
+#define CPP_DUMP_SET_OPTION(...)
+#define CPP_DUMP_DEFINE_EXPORT_OBJECT(...)
+#define CPP_DUMP_DEFINE_EXPORT_ENUM(...)
+#define CPP_DUMP_DEFINE_DANGEROUS_EXPORT_OBJECT(...)
+#endif
+
+#include "template.hpp"
+#include "utilities/fast_io.hpp"
+
+#endif  // M1UNE_PCH_HPP
